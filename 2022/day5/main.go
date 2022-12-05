a9f05ecc637c7274bbc542ca85a5d8efd72a711e
Dominykas <dominykas.petkevicius@gmail.com>
2022-12-05 09:46:36 +0200

    Part 2

--- 2022/day5/main.go
+++ 2022/day5/main.go
@@ -91,6 +91,73 @@ func part1() {
 }
 
 func part2() {
+	readFile, err := os.Open("input")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	stacks := [9][]byte{}
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		if len(line) == 0 {
+			for index, array := range stacks {
+				stacks[index] = reverse(array)
+			}
+		} else if line[0] == 'm' {
+			pairs := strings.Split(line, " ")
+
+			num, err := strconv.Atoi(pairs[1])
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			count := num
+
+			num, err = strconv.Atoi(pairs[3])
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			from := num - 1
+
+			num, err = strconv.Atoi(pairs[5])
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			to := num - 1
+
+			x := stacks[from][len(stacks[from])-count : len(stacks[from])]
+			stacks[from] = stacks[from][:len(stacks[from])-count]
+
+			stacks[to] = append(stacks[to], x...)
+		} else {
+			for i := 0; i <= (len(line)-1)/4; i++ {
+				if line[i*4+1] >= 65 && line[i*4+1] <= 90 {
+					stacks[i] = append(stacks[i], line[i*4+1])
+				}
+			}
+		}
+	}
+
+	result := ""
+
+	for _, array := range stacks {
+		result += string(array[len(array)-1])
+	}
+
+	readFile.Close()
+
+	fmt.Println(result)
 }
 
 func main() {
